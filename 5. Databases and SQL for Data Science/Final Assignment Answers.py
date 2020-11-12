44adede383e4e856f7fa1300fc9038958891df56
jamierjhill <68746997+jamierjhill@users.noreply.github.com>
2020-11-12 11:57:38 -0500

    Update Final Assignment Answers.py

--- 5. Databases and SQL for Data Science/Final Assignment Answers.py
+++ 5. Databases and SQL for Data Science/Final Assignment Answers.py
@@ -15,3 +15,13 @@
 
 #Q5. In the CENUS_DATA table list all community areas whose names start with the letter ‘B'
 %sql select community_area_name from socioeconomic where community_area_name like 'B%'
+
+#Q6. Problem 6: List the schools in Community Areas 10 to 15 that are healthy school certified?
+
+#Q7. What is the average school Safety Score?
+
+#Q8. List the top 5 Community Areas by average College Enrollments (number of students)
+
+#PQ9. Use a sub-query to determine which Community Area has the least value for Safety Score?
+
+#Q10. Find the Per Capita Income of the Community Area which has a school Safety Score of 1.
