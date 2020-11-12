daf0db0ab62c6a6e4d613156783d1bb707808d31
jamierjhill <68746997+jamierjhill@users.noreply.github.com>
2020-11-12 11:56:32 -0500

    Update Final Assignment Answers.py

--- 5. Databases and SQL for Data Science/Final Assignment Answers.py
+++ 5. Databases and SQL for Data Science/Final Assignment Answers.py
@@ -1,9 +1,17 @@
+#Need to add screenshots
+
 #Q1. Find the total number of crimes recorded in the Crime table Take a screenshot showing the SQL query and its results.
 %sql select count (*) as total_no_of_crimes from crimes
 533
 
 #Q2. Retrieve first 10 rows from the CRIME table
+%sql select * from Crimes limit 10
 
 #Q3. How many crimes involve an arrest?
+%sql select count(*) from crimes where ARREST = True
 
+#Q4. Which unique types of crimes have been recorded at a GAS STATION?
+%sql SELECT distinct(primary_type) FROM crimes WHERE location_description = 'GAS STATION'
 
+#Q5. In the CENUS_DATA table list all community areas whose names start with the letter ‘B'
+%sql select community_area_name from socioeconomic where community_area_name like 'B%'
